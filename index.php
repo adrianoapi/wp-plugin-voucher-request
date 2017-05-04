150b921799a6cdc1185ff9e4014e9686b876a756
Adriano A Costa <sdcomputadores@gmail.com>
2017-05-04 12:39:32 -0300

    add condição de select no form se consulta

--- index.php
+++ index.php
@@ -13,7 +13,6 @@ function voucher_register_table_results()
     #$table_name = "{$wpdb->prefix}posts";
     # 
     $curso = !empty($_POST['curso']) ? " AND cursos.id = " . $_POST['curso'] : NULL;
-    #$divulgador = !empty($_POST['divulgador']) ? " AND clientes.divulgador_id = " . $_POST['divulgador'] : NULL;
     $unidade = !empty($_POST['unidade']) ? ' AND unidades.id = ' . $_POST['unidade'] : NULL;
     $divulgador = !empty($_POST['divulgador']) ? " INNER JOIN divulgadores ON (clientes.divulgador_id = divulgadores.id AND clientes.divulgador_id = {$_POST['divulgador']})" : " LEFT JOIN divulgadores ON (clientes.divulgador_id = divulgadores.id)";
     # Query
@@ -55,6 +54,10 @@ function voucher_form_search()
     $divulgador = $wpdb->get_results("SELECT * FROM divulgadores");
     $cursos = $wpdb->get_results("SELECT * FROM cursos");
     $unidades = $wpdb->get_results("SELECT * FROM unidades");
+    # REQUEST POST
+    $r_divulgador = isset($_POST['divulgador']) ? $_POST['divulgador'] : NULL;
+    $r_curso = isset($_POST['curso']) ? $_POST['curso'] : NULL;
+    $r_unidade = isset($_POST['unidade']) ? $_POST['unidade'] : NULL;
     ?>
     <form action="" method="post">
         <div class="row">
@@ -64,7 +67,7 @@ function voucher_form_search()
                     <select name="divulgador" class="form-control" id="divulgador">
                         <option value="">Todos</option>
                         <?php foreach ($divulgador as $divulgador): ?>
-                            <option value="<?= $divulgador->id ?>"><?= $divulgador->nome ?></option>
+                            <option value="<?= $divulgador->id ?>" <?= $divulgador->id == $r_divulgador ? 'selected' : NULL ?>><?= $divulgador->nome ?></option>
                         <?php endforeach; ?>
                     </select>
                 </div>
@@ -75,7 +78,7 @@ function voucher_form_search()
                     <select name="unidade" class="form-control" id="unidade">
                         <option value="">Todas</option>
                         <?php foreach ($unidades as $unidade): ?>
-                            <option value="<?= $unidade->id ?>"><?= $unidade->nome ?></option>
+                            <option value="<?= $unidade->id ?>" <?= $unidade->id == $r_unidade ? 'selected' : NULL ?>><?= $unidade->nome ?></option>
                         <?php endforeach; ?>
                     </select>
                 </div>
@@ -86,7 +89,7 @@ function voucher_form_search()
                     <select name="curso" class="form-control" id="curso">
                         <option value="">Todos</option>
                         <?php foreach ($cursos as $curso): ?>
-                            <option value="<?= $curso->id ?>"><?= $curso->nome ?></option>
+                            <option value="<?= $curso->id ?>" <?= $curso->id == $r_curso ? 'selected' : NULL ?>><?= $curso->nome ?></option>
                         <?php endforeach; ?>
                     </select>
                 </div>
